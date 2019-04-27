f2b5c74acf0e0cf0b5dc03a2df90596c770e647b
Menen <menentechnologies@gmail.com>
2019-04-27 14:53:59 -0400

    ground and player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+
+    public float kForwardForce = 1.0f;
+    Vector2 forwardForce;
+    Vector2 upAxis = new Vector2(0f, 1f);
+    Vector2 rightAxis = new Vector2(1f, 0f);
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        bool keyRight = Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow);
+        bool keyLeft = Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow);
+        if (keyRight)
+        {
+            forwardForce = rightAxis * kForwardForce;
+        }
+        else if (keyLeft)
+        {
+            forwardForce = -rightAxis * kForwardForce;
+        }
+        else
+        {
+            forwardForce = new Vector2(0f, 0f);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(forwardForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+    }
+}
