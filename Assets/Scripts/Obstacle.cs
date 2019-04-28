aa8b826f1f4a61908ab83cf942762cb632a756d3
Rob Avery <robertaveryiv@gmail.com>
2019-04-28 12:59:42 -0400

    Flash player when lose life

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public bool isDestroyed;
+    public int state; // Reflects to what is in ObstacleState
+}
+
+enum ObstacleState {
+    Damage = 0,
+    Health = 1
+}
