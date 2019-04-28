0dabdbaca2557e5e86b8f99081a8582583e7058c
Menen <menentechnologies@gmail.com>
2019-04-27 21:42:35 -0400

    UI with events and end of level trigger

---
+++ Assets/GameEvents.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameEvents : MonoBehaviour
+{
+    public GameObject ScreenPlaying;
+    public GameObject ScreenPause;
+    public GameObject ScreenLevelComplete;
+    public GameObject ScreenPlayerDied;
+
+    void Start()
+    {
+        ScreenPlaying.SetActive(true);
+        ScreenPause.SetActive(false);
+        ScreenLevelComplete.SetActive(false);
+        ScreenPlayerDied.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        ScreenPlaying.SetActive(false);
+        ScreenPause.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1f;
+        ScreenPause.SetActive(false);
+        ScreenPlaying.SetActive(true);
+    }
+
+    public void LevelComplete()
+    {
+        Time.timeScale = 0f;
+        ScreenLevelComplete.SetActive(true);
+    }
+
+    public void PlayerDied()
+    {
+        Time.timeScale = 0f;
+        //show some canvas with options?
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene("MasterScene"); //TODO: change to load next level (where we have more levels)
+        Time.timeScale = 1f;
+    }
+
+    public void LoadSameLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    public void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene("MasterScene"); //TODO: change to load menu scene
+        Time.timeScale = 1f;
+    }
+}
