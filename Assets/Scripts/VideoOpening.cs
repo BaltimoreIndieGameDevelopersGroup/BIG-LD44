227186633b469a789b1768118808c5a34d79226d
Tony Li <tony@pixelcrushers.com>
2019-04-28 22:14:03 -0400

    Video prefabs

---
+++ Assets/Scripts/VideoOpening.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class VideoOpening : MonoBehaviour
+{
+    public Canvas coverCanvas;
+
+    IEnumerator Start()
+    {
+        coverCanvas.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.25f);
+        coverCanvas.gameObject.SetActive(false);
+        yield return new WaitForSeconds(3.5f);
+        gameObject.SetActive(false);
+    }
+}
